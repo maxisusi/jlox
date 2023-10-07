1b5a181019c5d0b7cff6d4b1b7030687279f8b3e
max <maxbastienbalej@gmail.com>
2023-10-07 10:59:35 +0200

    Basing implementation for single lexem

--- src/main.rs
+++ src/main.rs
@@ -33,12 +33,12 @@ enum TokenType {
 
 struct Token {
     lexeme: String,
-    line: u32,
+    line: usize,
     token_type: TokenType,
 }
 
 impl Token {
-    fn new(lexeme: String, line: u32, token_type: TokenType) -> Token {
+    fn new(lexeme: String, line: usize, token_type: TokenType) -> Token {
         Token {
             lexeme,
             line,
@@ -52,11 +52,12 @@ impl Token {
 }
 
 struct Scanner {
-    start: u32,
-    current: u32,
-    line: u32,
+    start: usize,
+    current: usize,
+    line: usize,
     source: String,
     tokens: Vec<Token>,
+    lexems: Vec<char>,
 }
 
 impl Scanner {
@@ -67,76 +68,74 @@ impl Scanner {
             line: 1,
             source,
             tokens: Vec::new(),
+            lexems: Vec::new(),
         }
+    }
+
+    fn scan_tokens(&mut self) -> &Vec<Token> {
+        // Transform source into list of chars
+        self.lexems = self.source.chars().collect();
+        while !self.is_end_file() {
+            self.start = self.current;
+            self.scan_token();
+        }
+
+        // Append EOF token to list
+        self.tokens
+            .push(Token::new(String::new(), self.line, TokenType::Eof));
+
+        return &self.tokens;
     }
 
     fn scan_token(&mut self) {
-        let file = self.source.chars();
-        for (idx, t) in file.enumerate() {
-            self.current = idx as u32;
+        let l = self.advance();
 
-            match t {
-                '/' => self
-                    .tokens
-                    .push(self.add_t(t.to_string(), self.line, TokenType::Slash)),
-                '(' => self
-                    .tokens
-                    .push(self.add_t(t.to_string(), self.line, TokenType::LeftParen)),
-                ')' => {
-                    self.tokens
-                        .push(self.add_t(t.to_string(), self.line, TokenType::RightParen))
-                }
-                '{' => self
-                    .tokens
-                    .push(self.add_t(t.to_string(), self.line, TokenType::LeftBrace)),
-                '}' => {
-                    self.tokens
-                        .push(self.add_t(t.to_string(), self.line, TokenType::RightBrace))
-                }
-                ';' => self
-                    .tokens
-                    .push(self.add_t(t.to_string(), self.line, TokenType::Semicolon)),
-                ',' => self
-                    .tokens
-                    .push(self.add_t(t.to_string(), self.line, TokenType::Comma)),
-                '.' => self
-                    .tokens
-                    .push(self.add_t(t.to_string(), self.line, TokenType::Dot)),
-                '-' => self
-                    .tokens
-                    .push(self.add_t(t.to_string(), self.line, TokenType::Minus)),
-                '+' => self
-                    .tokens
-                    .push(self.add_t(t.to_string(), self.line, TokenType::Plus)),
-                '*' => self
-                    .tokens
-                    .push(self.add_t(t.to_string(), self.line, TokenType::Star)),
-                '!' => self
-                    .tokens
-                    .push(self.add_t(t.to_string(), self.line, TokenType::Bang)),
-                '=' => self
-                    .tokens
-                    .push(self.add_t(t.to_string(), self.line, TokenType::Equal)),
-                '<' => self
-                    .tokens
-                    .push(self.add_t(t.to_string(), self.line, TokenType::Less)),
-                '>' => self
-                    .tokens
-                    .push(self.add_t(t.to_string(), self.line, TokenType::Greater)),
-                ' ' => {}
-                '\n' => self.line += 1,
-                _ => {
-                    println!("Unrecognized token: '{}' at line: '{}'", t, self.line);
-                }
-            };
-        }
-        // Adding the EOF token
-        self.tokens
-            .push(Token::new(String::from(""), self.line, TokenType::Eof));
+        match l {
+            // Tokens
+            '/' => self.define_t(TokenType::Slash),
+            '(' => self.define_t(TokenType::LeftParen),
+            ')' => self.define_t(TokenType::RightParen),
+            '{' => self.define_t(TokenType::LeftBrace),
+            '}' => self.define_t(TokenType::RightBrace),
+            ';' => self.define_t(TokenType::Semicolon),
+            ',' => self.define_t(TokenType::Comma),
+            '.' => self.define_t(TokenType::Dot),
+            '-' => self.define_t(TokenType::Minus),
+            '+' => self.define_t(TokenType::Plus),
+            '*' => self.define_t(TokenType::Star),
+            '!' => self.define_t(TokenType::Bang),
+            '=' => self.define_t(TokenType::Equal),
+            '<' => self.define_t(TokenType::Less),
+            '>' => self.define_t(TokenType::Greater),
+            // Ignore whitespace
+            '\t' | '\r' | ' ' => {}
+            // New line
+            '\n' => self.line += 1,
+            // Error
+            _ => {
+                return println!("Unrecognized token: '{}' at line: '{}'", l, self.line);
+            }
+        };
     }
 
-    fn add_t(&self, lexeme: String, line: u32, token_type: TokenType) -> Token {
-        return Token::new(lexeme, line, token_type);
+    fn advance(&mut self) -> char {
+        let res = self.lexems[self.current];
+        self.current += 1;
+        return res;
+    }
+
+    fn define_t(&mut self, token: TokenType) {
+        self.add_t(token);
+    }
+
+    fn add_t(&mut self, token: TokenType) {
+        let text = self.source[self.start..self.current].to_string();
+        let token = Token::new(text, self.line, token);
+        self.tokens.push(token);
+    }
+
+    fn is_end_file(&self) -> bool {
+        return self.current >= self.source.len() - 1;
     }
 }
 
@@ -155,10 +154,10 @@ fn main() {
         let mut scanner = Scanner::new(file);
 
         // Get the tokens list
-        scanner.scan_token();
+        let tokens = scanner.scan_tokens();
 
         // Print the tokens
-        for token in scanner.tokens {
+        for token in tokens {
             println!("{}, {:?}, {}", token.lexeme, token.token_type, token.line);
         }
     } else {
